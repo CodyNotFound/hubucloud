c9629d633c53bcf110f30705e7dd1666340f4d73
CodyNotFound <codynotfound@outlook.com>
2025-09-25 15:05:20 +0800

    feat: group

--- config/forum.ts
+++ config/forum.ts
@@ -6,93 +6,18 @@ export interface Group {
 
 export const groupList: Group[] = [
     {
-        name: '湖大萧云游戏交流群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=WG%2FPC3a9NJ4gFSqydYyZ%2BVTKU8DaLtWKhC7D2UUH2bBra0MTvzGIuDM1A43UOna%2F&busi_data=eyJncm91cENvZGUiOiI5NjYzMTgwNTIiLCJ0b2tlbiI6Im5Ib2hzMVIxenRmc3lyTTdQTWhES1ZVd2NCYlpCdUFBT01XUnhxcEthdHJheVdXYVFIa2tWd20xTGJlbFRKOHoiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=qAn81xkN5mqJ3u7iB6qyaOp5px0iKay6TCM0OKAC_nert60GTsDUSus5Jn_r8D2cgZ9IkAJ_6nvBATdrEBc1QQ&svctype=4&tempid=h5_group_info',
-        icon: 'Gamepad2',
+        name: '湖大阳逻交流群',
+        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=E7550vXJIfdNQ4vS%2BKpFJvqTvY7ve1hix2p3RYTYhJP57hzn8VjrJD%2FbfMdV0JSN&busi_data=eyJncm91cENvZGUiOiIxMDU4Mzk3MDE4IiwidG9rZW4iOiJYbGN1WHo4a0JHUlc0cGJETFZmRDFhUHMwS3lKVGZSL25oeUVvZ2grS0orQnBCMmJ2U1JIOUJWaWJIclphbmN5IiwidWluIjoiMTIwNTk1Nzk1MiJ9&data=nzr7Y825G2GnNONFbqdMyGPtWo88ebEw-gdLQEkOPv7SVLgsdsKkUoGXSc5JG3FCCVrXo2ZHOa-B-PzqSinWiA&svctype=4&tempid=h5_group_info',
+        icon: 'MapPin',
     },
     {
-        name: '湖大萧云代拿群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=oqL86qsBaXFgeRTzL7nGiQYEoVR%2F7z3u8vQFxXLz%2FFg%2BnzEAbdxgQm33Nfs%2FvP5H&busi_data=eyJncm91cENvZGUiOiI2MTg3NjE1ODAiLCJ0b2tlbiI6IlB1d2ZhYlB1K3FEZ01kMlg2MldYcnZMZURGZmZ4UWthMkFNYkhKY3VhdllPdE84aHpEU0NlK04zaVREYk9hTFoiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=bqvm7aYmZNHpHDUlX32Ac3UcUQZtLO-UaGW3YP5ei4uRya9Y9IeuxBLK18t6U-u6X0n2DifEsye81MmiAPkHdQ&svctype=4&tempid=h5_group_info',
-        icon: 'Package',
-    },
-    {
-        name: '湖大萧云桌游群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=QfOGWsTLa74Lm0SN2hJhesgWDC99HlTuUPNbfiVYcChjAd16DaQGDt%2Fboyy8TT6K&busi_data=eyJncm91cENvZGUiOiI3NTI0Mzc2ODEiLCJ0b2tlbiI6IlUyejU1YUpTK2EyYW0vWFBaRXlacHM1dFloTHZzeC9TZzYyR1pJREpPNXVJOVIzZ254bGEwUHhjVlFKMnZCamMiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=tyHRULCF63eXxxpnt4MjPnu1ZRX8-h-mPLjoQFzRG7PZKp0bWb3ygQpHJ-IKG2M_Jkcm1d-Mc1faLaFzGgMgfw&svctype=4&tempid=h5_group_info',
-        icon: 'Puzzle',
+        name: '湖大武昌交流群',
+        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=hsuEPPCYi4OL7SBPYOjUIavhhSOP7Oh3d9hjdYg%2FEbQ2X010mRSa13V35VDv7LSK&busi_data=eyJncm91cENvZGUiOiI2OTQxOTM1NDIiLCJ0b2tlbiI6Ik1DSHUydGtydlVjY1NEZjFaSGRHS0lBRWE4dXBWTXNDUHFsUjEvZ0JnUTZpVitDdkwvd1FBMU1jeWhEQmY2UzEiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=x_aKeEdLqStgETzm5leGKHfq24_XCNUbjX_Io7XRMzj61yNopu-7cUQSGhxDGUrx-_vZBwE_jE2qzVCEDqOgDQ&svctype=4&tempid=h5_group_info',
+        icon: 'MapPin',
     },
     {
         name: '湖大萧云拼车群',
         url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=qymsBcV5Nuh%2BxuZaJoYYpYhqk0imwDpicOcdqjxfE8uTJ5r4EGGgUZOevJsfeqXE&busi_data=eyJncm91cENvZGUiOiI4ODU4OTYzOTYiLCJ0b2tlbiI6IldnUFdpZzFmVzZvb1I2WEN0ZTM0enpiRTVSS1FMc295T3B5U3NsV3VFYmFWby9PREZ4ejh1UGxqRzYyUzh6VTQiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=FJUA315LKC06RWmqyE-OJm4Lwk7bgzwPCrHs85HFXZeMXwbZ8R8V0x-UJoyMjZjdcNUrXgEpUQInPTtmg3EtRQ&svctype=4&tempid=h5_group_info',
         icon: 'Car',
-    },
-    {
-        name: '湖大萧云健身群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=D0meijXL7xb8wukeKppHcBPD9o%2BzlLXB7T0a3jIjA90jgqaGjIzUhB%2BzOCt%2FIfEa&busi_data=eyJncm91cENvZGUiOiIyOTg2NTc5MDIiLCJ0b2tlbiI6IitkWUxLZ0JydjNUYjVjZllPK1NoWXZrcm1oQzRaWmp5L2JHL0pDdFpBUEZRU1lBWGhIanJXMzJON2pVeXFkSmwiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=BPYF6k5qe1MTubPqBfLwa99jdWvByViubPY24mWUiUmil2pire-COMxoLVMKJthqboq5hdhnHkE8GA7xDsWZSw&svctype=4&tempid=h5_group_info',
-        icon: 'Dumbbell',
-    },
-    {
-        name: '湖大萧云羽球群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=iofBHM5vYuMBIhqjS6A58jpOx7i%2BLxlk2qTGozI9qcZNlano0yklckgfCfF%2F6wuz&busi_data=eyJncm91cENvZGUiOiI5NzY5MjkxODQiLCJ0b2tlbiI6IkZJMU5ZTWZocGF5aGZYa0JqdXQ5QjJmcVRjSndyNVNLZlJlQ2RFblVhYkhrcW53RUlCQzlkMUpoRGs4ajNBREsiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=OZHObSl8sM26yiUkic0HjDjakPNulaks9H5879Vkla45Abitwi4xO8ryWHlKjH2BchAdbY6YrzGOkkO41ly_Ng&svctype=4&tempid=h5_group_info',
-        icon: 'Activity',
-    },
-    {
-        name: '湖大萧云篮球群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=FrFpAksPVKwXjX55YL%2FBmSkzJxUwIBXdfZbeArElytcWT7VIeGmhLvxR4lpW0bTH&busi_data=eyJncm91cENvZGUiOiIxNjY0NTM2NjYiLCJ0b2tlbiI6IjdkaUttM2FtWW9nZnVQTy9tMmgxLzJISE9NcWlTcjR0bTQ5eVFPSEpyVXBVY3FhQmlTZjQrUFV2Nmh1eElNbmciLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=QHpoedaXl-AimNfY3Tu6PD3iSJy-PAbCn0xaM87Oonw_T9YzPpXNjh50ILwV488SpZ96h2uB_jvOvH0fviC6Tg&svctype=4&tempid=h5_group_info',
-        icon: 'Circle',
-    },
-    {
-        name: '湖大萧云恋爱交友群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=X6zfd2GEgdlq%2BcjEjfjTFYs9UmZZRuA6V5BVpucnyV5t6H2b7L8o46x2%2B18kNBS5&busi_data=eyJncm91cENvZGUiOiIxNzQ1NzE4MDUiLCJ0b2tlbiI6Inh3NUhkLzJlU2RITis3UDZ2NnN6QmhpamUrV2F6OExucG9RNFpnUzBzQko1WG1UZXRZRFc5eU1wWkMrS2xRRmsiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=TIOMMlIS0UBbHv-XsIHka9opWgwQTDKLWkZXmGbSOM5a2ewuONSuP68my5FfpZSALK3vvcG08rgB-TdGKbeYPw&svctype=4&tempid=h5_group_info',
-        icon: 'Heart',
-    },
-    {
-        name: '湖大萧云足球群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=HKwE%2ByG5Z4j3%2BsIZrpYl%2Bcmysr03xmN64ROnYGq%2BARcp9eCEECSZ%2B8HALqBVOgW9&busi_data=eyJncm91cENvZGUiOiIyNjAzMzk3MzMiLCJ0b2tlbiI6IlJOWTlnSXFjeVYrQVBEdFMyQVNMMkdHUzZ1OVFkZE1rTmFGSHJiSjgrNEtEaVBXUHB3R0wxbyswaHFidjRyeUoiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=pxRRTL7zgoQP46BJh_CvBP9JR6SAzjbj_LUnQVwOiPGOIdbBU22s-akF8D0owEVGfExr23POlLHhyUfOWESAWQ&svctype=4&tempid=h5_group_info',
-        icon: 'Target',
-    },
-    {
-        name: '湖大萧云ktv群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=1X%2FjPyg3LZDFClPF2b4AGEiVCFLEMpgRAhCsFafnqu0zucv9BLlUBJN9Oompymuo&busi_data=eyJncm91cENvZGUiOiI5OTQ5MDM0NTEiLCJ0b2tlbiI6IldJZGlaZHJBU3FGV2tPV2RDSUVqZUo3TWdQOVdtQk1PeHlCWGVUdEJqMStqaGxDUG9KOHZkbTY1Y09QUFQ2ZlAiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=jSODHIw6mguihtUAALhD5PsLNG7yqW_UQem0O21Arw94ccNJOJ_xzYvTFKIkoF6_Utl2n7AA8zG-qxh0PGjfOg&svctype=4&tempid=h5_group_info',
-        icon: 'Mic',
-    },
-    {
-        name: '湖大萧云声乐群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=3ZHMPXmS6HpbrGumnshRTDfrgoUrWudmV%2FloiFLlf05Gf%2BWuHLpG3XWyerrAIup1&busi_data=eyJncm91cENvZGUiOiI2NjkxNTQzNjciLCJ0b2tlbiI6IkNYUk5mTm9sK1pCYUVpU0U2T001ZElPbHlyWXJCMVlxY05BUHVGd29uRVVrZUQ0UCsrNnhxLzY2eUM5anRqbTEiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=CeePUO0BfmhOpGKgCVMlybFjutV1Jld8Wsy03x2d3XcNH8uWoipR6cXOuZYtlKtu4DpbxXmpUTuKDIX5Vt1ZiQ&svctype=4&tempid=h5_group_info',
-        icon: 'Music',
-    },
-    {
-        name: '湖大萧云电影群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=Lg9bwM1wit6G5R7U07OL6cXtSIve5rOGeNqcu5jamn6oVt5tkVZ51Zc687dUea8m&busi_data=eyJncm91cENvZGUiOiI4MzA2Njg5MzkiLCJ0b2tlbiI6IlE1THptM0xiMjdTaWtrQkY1eFkrN1B4OVpNaUhVVkhKYTdMS0ZhUlVvQi83TUVQaERZM0RIV2FFSHRURDVhc24iLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=TGsj853RrmOrE3WB9-jlE2urHPGl4Iwrdm58CeJ7sZ6fpc2XGk80nEQrcDJrU2MSEFlCfuTML1P4JlOlwGl8Gw&svctype=4&tempid=h5_group_info',
-        icon: 'Film',
-    },
-    {
-        name: '湖大萧云台球群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=Ztv3JSexJJge55KRPmDgL%2BBH3qUcRD%2BcbOWfBrh2ePa7%2FaWOVJsS0O43w1kaPgju&busi_data=eyJncm91cENvZGUiOiI1NDQxNjE0NzUiLCJ0b2tlbiI6InJ4LzQzZEJicXc0NTVaOFZvd0xIYWpkRlZJSTZ4UXdOYWpNTkpyemFoanA5dUUrbHUvWUFYWFRUTmlSNlpJOTkiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=R1nVblDZPy7L_pY51Z1CL8DfSaCwvzi93uf4plqJaSuLoLxQNmpzjmvdqWqwZyTIFCvRpqQdO3tOWJKeVgpZIw&svctype=4&tempid=h5_group_info',
-        icon: 'Crosshair',
-    },
-    {
-        name: '湖大萧云乒乓球群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=O4JLzXx%2FQguswAkSxJIMfj%2FILkb7vO2GLaYzwTaZGqkZTgy8n5STSKaFLMwMB%2B1F&busi_data=eyJncm91cENvZGUiOiIzNjY5MzgyNzEiLCJ0b2tlbiI6IkYyZzhmLzdzMlkyTXNBbURDQTFqblE2WEVDUmM5bWZ4SUFUYnA4ZVV5WmNxQzJyWDdUeFZKMTh6Sk5DS214bHAiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=xb8dEbbkoai7CjAsxF5kWX7NXl3eadCUPShHHageGjGrbLdf_BTYHel_y1jG7za53_oMRgynHkhzfT-6s4N2kA&svctype=4&tempid=h5_group_info',
-        icon: 'Circle',
-    },
-    {
-        name: '湖大萧云密室逃脱群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=vixSFYqzOZvHBMaRBEZh5hjiAxxcmyzdwJGSrWL7AiYaImwat4d7%2Bgkr%2BCYmEG%2B%2B&busi_data=eyJncm91cENvZGUiOiI4MjkyNDQzNDkiLCJ0b2tlbiI6InkwcGxmZ0lmRTNXcWRnTllicUdObllsYzdtV0FUb0s3b2VYMThYcFJ6S1k4TTAvRmtwVFp3REJDYzM0a1hqdXoiLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=Nf8v6zmkoa2uL5UJ6Eu2LqZnQfolLsbw5COyMlnFtaL-TFF6LNznNlpFmjf5jHTt4XMHgpDrDP1W25kAOJjsjw&svctype=4&tempid=h5_group_info',
-        icon: 'Lock',
-    },
-    {
-        name: '湖大萧云练车群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=qSiDGe8FLZA2AGByhUizdFJy2DIrCZdbC1b29Qmo9DKzrDueZrjj9KOh1eSoveTz&busi_data=eyJncm91cENvZGUiOiI4MTIyNzU5NTUiLCJ0b2tlbiI6ImR2Q28xMUN6dndBQXhuSTJBQVI5Nk9NaVJoZGgyNFNlV05FLzQrQnBMRWpRaEZ6RGk4dTBZcEo0VThiVTdSZ1ciLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=5NuxAjQAH5wpFzPe2fcNQXjOghVktnRMNhuHCekvgosOsMIjyCk6yh8JPDknE47egkjkGTcqdsVXXToKYGdtWQ&svctype=4&tempid=h5_group_info',
-        icon: 'Car',
-    },
-    {
-        name: '湖大萧云网吧群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=u%2BlXSzWnWXTg2vc5l3Ikx2jTxAHFNZJnDagoixLQRJMNa4Y67A7NqeXI%2Bs2Da1GN&busi_data=eyJncm91cENvZGUiOiI4OTI2NjcyMjMiLCJ0b2tlbiI6Ik1JRkR0WWpxdGlIK1dzNml4bGhMbnRrd2VSdyt5MjhOSzNibCs0ckU0cGVXQWRpWjdXa1YwZER0d1U3VDNoUFciLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=9zZtBGSPbrlHdUOQ59puqaVqnfedaE5wUJXXpzJyrY0mEVUxSySnW6opwlEkXSDetBa4OTwTYEwIu_KN9f3T5Q&svctype=4&tempid=h5_group_info',
-        icon: 'Wifi',
-    },
-    {
-        name: '湖大萧云排球群',
-        url: 'https://qun.qq.com/universal-share/share?ac=1&authKey=y7qD8K0hzoU16vdhIX2rZH7UhWXLT79H5feeJOOa9sbMJ4zSew2wikBMwM3edXXZ&busi_data=eyJncm91cENvZGUiOiIyMDMxMDg1OTMiLCJ0b2tlbiI6ImF0QTNIWDJ0cWlJbERpWEI1SFR0dXBmNkp4bHZuSm0ya2xuQkVzMGRHYWNwMWliM3p0S2V3cThHSzZTWnp6UW0iLCJ1aW4iOiIxMjA1OTU3OTUyIn0%3D&data=ld7WlNvf0pkAErZmOCr1mZ_i4b8iN7M1c2x5bvdD78MOgfMuxWUFMR5wMgfpNwCrX3GZzQsljYgyigCWOMjHBw&svctype=4&tempid=h5_group_info',
-        icon: 'Zap',
     },
 ];
